c9db5b6f617f6297a635443a7810d77f274dc4da
Gustavo Ferreira <102172136+gustapng@users.noreply.github.com>
2022-08-21 11:24:13 -0300

    arquivos de dados

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,49 @@
 <?php
 
-require "../../../app_help_desk/valida_login.php";
+    session_start();
+
+    // variavel que verifica se a autenticação foi realizada
+    $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = [1 => 'Administrativo', 2 => 'Usuário'];
+
+    // usuarios do sistema
+    $usuarios_app = [
+        [
+        'id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1
+        ],
+        [
+        'id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1
+        ],
+        [
+        'id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2
+        ],
+        [
+        'id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2
+        ]
+    ];
+
+    foreach($usuarios_app as $user) {
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
+        }
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuário autenticado';
+        $_SESSION['autenticado'] = 'SIM';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
+        header('Location: home.php');
+    } else {
+        echo 'Erro na autenticação do usuário';
+        $_SESSION['autenticado'] = 'NÃO'; 
+        header('Location: index.php?login=erro');
+    }
+    
 
 ?>
